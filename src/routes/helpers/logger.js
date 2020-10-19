8f12ab1d62b528071eba9108562e78a3847cc6c4
Oleg Gaidukevich <oleg.g@intelico.su>
2020-10-19 20:06:12 +0300

    change log colors

--- src/routes/helpers/logger.js
+++ src/routes/helpers/logger.js
@@ -3,10 +3,13 @@ const morgan = require('morgan');
 
 const router = new Router();
 
-morgan.token('body', req => `body: ${JSON.stringify(req.body)}`);
+morgan.token(
+  'body',
+  req => `\x1b[45m \x1b[30m body: ${JSON.stringify(req.body)} \x1b[40m \x1b[37m`
+);
 morgan.token(
   'params',
-  req => `\x1b[36m params: ${JSON.stringify(req.params)} \x1b[0m`
+  req => `\x1b[42m params: ${JSON.stringify(req.params) || '{}'} \x1b[0m`
 );
 
 router.use(morgan(':method :url :params :body :status :response-time ms'));
